694995c945bfa9323029c72c91763e3f056db43c
zzerowin <alaykr123@gmail.com>
2022-05-10 17:48:40 +0900

    Recap

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -8,11 +9,23 @@ const H1 = styled.h1`
   color: ${props => props.theme.textColor};
 `
 
-function App() {
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
 
+function Dummy({ text, active = false }: DummyProps) {
+  return <h1>{text}</h1>
+}
+
+function App() {
+  const onClick = (e:React.MouseEvent<HTMLButtonElement>) => {
+  }
   return (
     <Container>
       <H1>protected</H1>
+      <Dummy active text="hello" />
+      <button onClick={onClick}>Click me</button>
     </Container>
   );
 }
