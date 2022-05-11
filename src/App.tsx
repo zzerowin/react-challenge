8a06ba3775cd6c0303f8f565769589d78d8007a3
zzerowin <alaykr123@gmail.com>
2022-05-11 12:02:18 +0900

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
 import React from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${props => props.theme.bgColor};
-`
-
-const H1 = styled.h1`
-  color: ${props => props.theme.textColor};
-`
-
-interface DummyProps {
-  text: string;
-  active?: boolean;
-}
-
-function Dummy({ text, active = false }: DummyProps) {
-  return <h1>{text}</h1>
-}
+import Router from "./Router";
 
 function App() {
-  const onClick = (e:React.MouseEvent<HTMLButtonElement>) => {
-  }
+
   return (
-    <Container>
-      <H1>protected</H1>
-      <Dummy active text="hello" />
-      <button onClick={onClick}>Click me</button>
-    </Container>
+    <Router />
   );
 }
 
