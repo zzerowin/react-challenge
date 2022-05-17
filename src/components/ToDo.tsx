3cea286a9d219ab53a188ab74bfbd5c648a9a00e
zzerowin <alaykr123@gmail.com>
2022-05-17 14:02:03 +0900

    5.12 Refactoring

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { IToDo } from "../atoms";
+
+function ToDo({ text }: IToDo) {
+
+  return (
+    <li>
+      <span>{text}</span>
+      <button>To Do</button>
+      <button>Doing</button>
+      <button>Done</button>
+    </li>
+  )
+}
+
+export default ToDo;
