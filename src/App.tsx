870721a92100a8016336e3e528745ff6b9c2ec79
zzerowin <alaykr123@gmail.com>
2022-05-19 15:59:26 +0900

    7.6 Gestures part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,6 +9,16 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, .4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const Box = styled(motion.div)`
   width: 200px;
@@ -20,18 +31,25 @@ const Box = styled(motion.div)`
 const boxVariants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: '100px' },
-  drag: { backgroundColor: "rgb(0, 168, 255)", transition: { duration: 10 } },
 }
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+
   return (
     <Wrapper>
-      <Box
-        drag variants={boxVariants}
-        whileHover="hover"
-        whileDrag="drag"
-        whileTap="click"
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
