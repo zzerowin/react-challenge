93dd39a1c08f87c95a14735e99e00cda30caa021
zzerowin <alaykr123@gmail.com>
2022-05-19 17:48:27 +0900

    7.7 MotionValues part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
+import { log } from "util";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -10,16 +11,6 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, .4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
@@ -28,28 +19,12 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: '100px' },
-}
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
-
+  const x = useMotionValue(0);
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={0.5}
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileHover="hover"
-          whileDrag="drag"
-          whileTap="click"
-        />
-      </BiggerBox>
+      <Box style={{ x }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
